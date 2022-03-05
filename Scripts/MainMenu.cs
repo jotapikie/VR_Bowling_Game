1543429e4680eae06b70805893df29564c83f1f8
João Pedro Silva <jotapikie@gmail.com>
2022-03-05 18:48:31 +0000

    Add files via upload

---
+++ Scripts/MainMenu.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public int Score;
+    public void LoadGame()
+    {
+        Score=0;
+        SceneManager.LoadScene(1);
+    }
+
+}
